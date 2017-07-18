1385ea15857c786c89ae33f0be7756c0aad33a54
maryam aminpour <maryamdehghan93@gmail.com>
2017-07-18 13:26:59 -0700

    home page

---
+++ git.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <title>My page</title>
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    </head>
+  <body>
+  <div class="jumbotron text-center">
+  <h1> Start your search here</h1>
+  <form class="form-inline">
+    <div class="input-group">
+      <input type="search" class="form-control" id="search-box" size="70" placeholder="Search stores..." required>
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-success btn-lg">Search</button>
+      </div>
+    </div>
+  </form>
+  </div>
+
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="http://localhost/css-git.css">
+   </body>
+  </html>
